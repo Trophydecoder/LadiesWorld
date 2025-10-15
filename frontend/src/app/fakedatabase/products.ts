9b269e1fe9b888b2d9a1c74da4d8c49be28adb8e
Trophydecoder <trophydecoder@gmail.com>
2025-10-15 18:32:11 +0200

    shop done, contact , cart , products description left

--- frontend/src/app/fakedatabase/products.ts
+++ frontend/src/app/fakedatabase/products.ts
@@ -1,6 +1,6 @@
 export const products = [
     {
-        image: 'assets/waxstick.jpg',
+        image: 'assets/waxstick.png',
         "productname" : "Wax Stick" , 
         "price" : 80,
         "description": "A Wax Stick is a versatile hair styling product designed to provide smooth, long-lasting hold with a natural finish. Ideal for taming flyaways, defining edges, or adding shine to hair, it’s easy to apply and works on all hair types. Perfect for quick touch-ups or detailed styling, it keeps hair looking neat and polished throughout the day",
@@ -8,7 +8,7 @@ export const products = [
 
     },
     {
-        image: 'assets/edgebrush.jpg',
+        image: 'assets/edgebrush.png',
         "productname" : "Edge Brush" , 
         "price" : 20,
         "description": "An Edge Brush is a precision styling tool designed to smooth, shape, and define the edges and baby hairs around your hairline. Its firm yet flexible bristles make it perfect for creating sleek, polished looks, controlling flyaways, and achieving detailed hairstyles with ease. Ideal for all hair types and essential for finishing any hairstyle",
@@ -18,7 +18,7 @@ export const products = [
 
 
     {
-        image: 'assets/elasticband.jpg',
+        image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -27,7 +27,7 @@ export const products = [
     },
 
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -36,7 +36,7 @@ export const products = [
     },
 
       {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -44,7 +44,7 @@ export const products = [
 
     },
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -52,7 +52,7 @@ export const products = [
 
     },
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -60,14 +60,14 @@ export const products = [
 
     },
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
         "instock" : 30
     },
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -75,7 +75,7 @@ export const products = [
 
     },
     {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" :40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -83,7 +83,7 @@ export const products = [
 
     }
     ,    {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" : 40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
@@ -91,7 +91,7 @@ export const products = [
 
     }
     ,    {
-        image: 'assets/elasticband.jpg',
+      image: 'assets/elasticband.png',
         "productname" : "Elastic Band" , 
         "price" : 40,
         "description": "An Elastic Band is a versatile hair accessory designed to securely hold hair in place without causing damage or breakage. Perfect for ponytails, braids, buns, or everyday hairstyles, it stretches to fit comfortably while maintaining strong hold. Ideal for all hair types, it helps keep styles neat and polished throughout the day",
